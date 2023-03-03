a6e002bf6fb37859e8181d0eb8b714f75ed28673
David Oliveira <daviddenisson2014@gmail.com>
2023-03-03 20:10:21 -0300

    finalizando

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,9 +1,25 @@
 import React from 'react'
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLinkedinIn, faGithubAlt, faInstagram } from '@fortawesome/free-brands-svg-icons'
+import logo from '../assets/logo.png'
 const Footer = () => {
   return (
     <footer>
-        
+
+
+      <div className="social-medias">
+        <ul>
+          <li><a href="#"><FontAwesomeIcon icon={faLinkedinIn} /></a></li>
+          <li><a href="#"><FontAwesomeIcon icon={faGithubAlt} /></a></li>
+          <li><a href="#"><FontAwesomeIcon icon={faInstagram} /></a></li>
+        </ul>
+      </div>
+
+
+      <div className="copyright">
+        Developed by David Oliveira
+      </div>
+
     </footer>
   )
 }
