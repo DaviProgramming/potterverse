bef812c28786e75dd93fd12b096042ba01430da2
David Oliveira <daviddenisson2014@gmail.com>
2023-03-02 21:13:14 -0300

    ajustes e iniciando validação

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,8 +1,19 @@
-import React from 'react'
+import { React,  useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPaperPlane } from '@fortawesome/free-solid-svg-icons'
 import heroImage from '../assets/harry-wallpaper-2.jpg';
 const Hero = () => {
+
+
+    const [inputValue, setInputValue] = useState("");
+
+    const validations = {
+        inputValidation(value){
+
+        }
+    }
+
+
     return (
         <section className='container hero'>
             <div className="content-hero">
@@ -10,9 +21,13 @@ const Hero = () => {
                     <div className="hero-text">Receba as mais diversas informações sobre <span>Harry potter</span></div>
                     <form>
                         <div className="form-control">
-                            <input type="text" placeholder='Digite seu melhor Email' />
-                            <button className='button' type='button'><FontAwesomeIcon icon={faPaperPlane} /></button>
+                            <input type="text" placeholder='Digite seu melhor Email' onChange={e => setInputValue(e.target.value)} />
+                            
+                             <span className='input-error'>Digite um Email valido !</span>
                         </div>
+
+                        <button className='button' type='button' onClick={(e)=>{console.log(inputValue)}}><FontAwesomeIcon icon={faPaperPlane} /></button>
+
 
                     </form>
                 </div>
