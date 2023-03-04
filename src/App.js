dbb21fae94b9872b6006ce47724da0a98f6517dc
David Oliveira <daviddenisson2014@gmail.com>
2023-03-04 13:09:10 -0300

    removendo seções desnecessarias

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import Hero from './components/hero';
-import Navbar from './components/navbar';
+
 import About from './components/about';
-import LastNews from './components/lastNews';
+
 import Footer from './components/footer';
 function App() {
   return (
     <div className="App">
-      <Navbar />
+    
       <Hero />
       <About />
-      <LastNews />
       <Footer />
     </div>
   );
